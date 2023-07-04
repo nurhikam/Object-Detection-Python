0c869e3324ba18fd4ee135978fcdb0f4ce91ef11
nurhikam <nurhikam65@gmail.com>
2023-07-04 20:44:14 +0700

    adding images

--- Running YOLO/Yolo-Basics.py
+++ Running YOLO/Yolo-Basics.py
@@ -2,5 +2,5 @@ from ultralytics import YOLO
 import cv2
 
 model = YOLO('../Yolo-Weights/yolov8l.pt')
-results = model("Images/3.png", show=True)
+results = model("Images/4.png", show=True)
 cv2.waitKey(0)
